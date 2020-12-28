c7245e0c669a6b4e78d5c315b1b33dd9bf07ce4e
prawar <prawarmundra20@gmail.com>
2020-12-29 02:01:59 +0530

    init

---
+++ src/components/Layout/Layout.js
@@ -0,0 +1,15 @@
+//import classes from '*.module.css';
+import React from 'react';
+import Aux from '../../hoc/Aux';
+import classes from './Layout.module.css'
+
+const layout = (props) => (
+    <Aux>
+        <div>Toolbar, sideDrawer, Backdrop</div>
+        <main className = {classes.Content}>
+            {props.children}
+        </main>
+    </Aux>
+)
+
+export default layout;
