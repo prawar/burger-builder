80ab21686b6835905f7da3cb2e8d3c573d95e455
prawar <prawarmundra20@gmail.com>
2020-12-30 19:12:47 +0530

    Adding Continue and Cancel Button to Summary page

--- src/components/Burger/OrderSummary/OrderSummary.js
+++ src/components/Burger/OrderSummary/OrderSummary.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import Button from '../../UI/Button/Button';
 
 import Aux from '../../../hoc/Aux'
 const orderSummary = (props) => {
@@ -18,6 +19,8 @@ const orderSummary = (props) => {
                     {ingredientSummary}
                 </ul>
                 <p>Continue with checkout?</p>
+                <Button btnType='Danger' clicked={props.purchaseCancelled}>CANCEL</Button>
+                <Button btnType='Success' clicked={props.purchaseContinued}>CONTINUE</Button>
             </Aux>
         )
 
