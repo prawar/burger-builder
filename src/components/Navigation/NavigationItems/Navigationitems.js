997ad4ce1cd4c8c90dba6fa3fbbc64edc951dfe4
prawar <prawarmundra20@gmail.com>
2021-01-02 15:43:25 +0530

    Adding a toolbar, a sidedrawer, a backdrop

---
+++ src/components/Navigation/NavigationItems/Navigationitems.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import classes from './Navigationitems.module.css';
+import NavigationItem from './Navigationitem/Navigationitem'
+
+const navigationItems = () => (
+    <ul className={classes.NavigationItems}>
+        <NavigationItem link="/" active>Burger Builder</NavigationItem>
+        <NavigationItem link="/" >Checkout</NavigationItem>
+        
+    </ul>
+)
+
+export default navigationItems;
