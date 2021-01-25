6ee8f376bd304345be9d08987a318362d13a63d2
prawar <prawarmundra20@gmail.com>
2021-01-25 21:48:59 +0530

    adding a contact form

--- src/containers/Checkout/ContactData/ContactData.js
+++ src/containers/Checkout/ContactData/ContactData.js
@@ -3,6 +3,7 @@ import Button from '../.././../components/UI/Button/Button';
 import Spinner from '../../../components/UI/Spinner/Spinner';
 import classes from './ContactData.module.css';
 import axios from '../../../axios-orders';
+import Input from '../../../components/UI/Input/Input';
 
 class ContactData extends Component {
     state = {
@@ -45,10 +46,10 @@ class ContactData extends Component {
 
     render() {
         let form = (<form>
-            <input className={classes.Input} type="text" name="name" placeholder="Your Name" />
-            <input className={classes.Input} type="text" name="email" placeholder="Your email" />
-            <input className={classes.Input} type="text" name="street" placeholder="street" />
-            <input className={classes.Input} type="text" name="postal" placeholder="Postal Code" />
+            <Input inputtype="input" type="text" name="email" placeholder="Your email" />
+            <Input inputtype="input" type="text" name="name" placeholder="Your Name" />
+            <Input inputtype="input" type="text" name="street" placeholder="street" />
+            <Input inputtype="input" type="text" name="postal" placeholder="Postal Code" />
             <Button btnType="Success" clicked={this.orderHandler}>ORDER</Button>
         </form>);
         if(this.state.loading) {
